f71d1c6ae40f8306ce8662a3fa4625c45eb3d971
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-05-09 14:13:18 -0700

    commit tree 5b3f7e5e8fd8fe0a8447dc4bf11df30951387ebc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |person|
+        puts person
+    end
+    assign_rooms(names).each do |rooms|
+        puts rooms
+    end
+end
